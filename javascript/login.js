3ae04f473c43c1c3bc3b89e05e2f36e957de0051
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-10-06 17:06:58 +0300

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,17 @@
+
+const adminUsername = "admin";
+const adminPassword = "admin123";
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === adminUsername && password === adminPassword) {
+        // Redirect to admin dashboard
+        window.location.href = 'admin_dashboard.html';
+    } else {
+        document.getElementById('error-message').innerHTML = 'Invalid credentials. Please try again.';
+    }
+});
